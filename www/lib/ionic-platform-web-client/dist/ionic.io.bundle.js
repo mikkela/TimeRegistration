33baa7203341a884b3d068f0613fd19ba9691198
Mikkel Arentoft <mail@mikkelarentoft.net>
2016-02-09 20:09:54 +0100

    Created security profile

--- www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js
+++ www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js
@@ -3439,7 +3439,7 @@ var BaseSettings = (function () {
 })();
 
 var temp = new BaseSettings().factory('$ionicCoreSettings', function () {
-  "IONIC_SETTINGS_STRING_START";var settings = {"dev_push":true}; return { get: function(setting) { if (settings[setting]) { return settings[setting]; } return null; } };"IONIC_SETTINGS_STRING_END";
+  "IONIC_SETTINGS_STRING_START";var settings = {"dev_push":true,"app_id":"c72688c8"}; return { get: function(setting) { if (settings[setting]) { return settings[setting]; } return null; } };"IONIC_SETTINGS_STRING_END";
 }).finish();
 
 var Settings = (function () {
